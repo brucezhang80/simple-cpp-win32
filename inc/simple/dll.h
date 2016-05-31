38b21f796300bb10ad06883e656a787b7f2968fc
ximenpo <ximenpo@jiandan.ren>
2016-05-31 18:39:35 +0800

    fix enum names.

--- inc/simple/dll.h
+++ inc/simple/dll.h
@@ -5,14 +5,14 @@
 
 enum DCSResult
 {
-	CFES_OK, 
-	CFES_MissingFunctions,
-	CFES_ErrorOpeningFile,
-	CFES_ErrorReadingFile,
-	CFES_InvalidDosHeader,
-	CFES_InvalidNTHeader,
-	CFES_NotDLL,
-	CFES_DLLStructureError,
+	DCS_OK, 
+	DCS_MissingFunctions,
+	DCS_ErrorOpeningFile,
+	DCS_ErrorReadingFile,
+	DCS_InvalidDosHeader,
+	DCS_InvalidNTHeader,
+	DCS_NotDLL,
+	DCS_DLLStructureError,
 };
 	
 // Checks if the specified file is a real DLL that exports all 
