704ae458a3b7d5c9b3a03a3968b81e754b45d667
ximenpo <ximenpo@jiandan.ren>
2016-06-03 16:24:30 +0800

    add res.cpp/h

---
+++ inc/simple/res.h
@@ -0,0 +1,7 @@
+#ifndef RES_H_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+#define RES_H_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+
+bool Res_ReplaceICO		(const char* pszModule, const char* sResID, const char* lpIconFile);
+bool Res_ReplaceString	(const char* pszModule, int nResID, const char* pszText);
+
+#endif
