820dac1663e864c9547ad3fad98e39a3771be9c1
ximenpo <ximenpo@jiandan.ren>
2016-06-03 18:08:58 +0800

    add anti.h

--- inc/simple/patcher.cpp
+++ inc/simple/patcher.cpp
@@ -2,7 +2,7 @@
 #include	"patcher.h"
 
 #if	!defined(HEAP_CREATE_ENABLE_EXECUTE)
-#define	HEAP_CREATE_ENABLE_EXECUTE 0x00040000 
+#define	HEAP_CREATE_ENABLE_EXECUTE 0x00040000
 #endif
 
 #include	"_third/CPatch/patcher.cpp"
