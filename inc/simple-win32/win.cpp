f277474bb7305ac561ad7984a82d7e5531ffa23b
ximenpo <ximenpo@jiandan.ren>
2016-06-17 11:09:55 +0800

    add win.h/cpp

---
+++ inc/simple-win32/win.cpp
@@ -0,0 +1,45 @@
+﻿#include	<Windows.h>
+#include	<tchar.h>
+
+//
+//	判断是否64位系统
+//
+bool	Win_Is64bitsSystem() {
+    typedef VOID (WINAPI *LPFN_GetNativeSystemInfo)(LPSYSTEM_INFO lpSystemInfo);
+
+    SYSTEM_INFO si;
+    LPFN_GetNativeSystemInfo fnGetNativeSystemInfo = (LPFN_GetNativeSystemInfo)GetProcAddress( GetModuleHandle(_T("kernel32")), "GetNativeSystemInfo");;
+    if (NULL != fnGetNativeSystemInfo) {
+        fnGetNativeSystemInfo(&si);
+    } else {
+        GetSystemInfo(&si);
+    }
+    if (si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_AMD64 ||
+            si.wProcessorArchitecture == PROCESSOR_ARCHITECTURE_IA64 ) {
+        return true;
+    }
+
+    return false;
+}
+
+//
+//	根据类名获取子窗口，同 FindWindowEx
+//
+HWND	Win_FindChildByClass(HWND hWnd, const char* sClass) {
+    HWND	hChild	= GetWindow(hWnd, GW_CHILD);
+    if(NULL == hChild) {
+        return	hChild;
+    }
+
+    do {
+        char	buf[MAX_PATH]	= {};
+        GetClassName(hChild, buf, sizeof(buf) - 1);
+        if(0 == _strcmpi(buf, sClass)) {
+            return	hChild;
+        }
+
+        hChild	= GetWindow(hChild, GW_HWNDNEXT);
+    } while(hChild != NULL);
+
+    return	NULL;
+}
---
+++ inc/simple-win32/win.h
@@ -0,0 +1,14 @@
+﻿#ifndef WIN_H_WIN_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+#define WIN_H_WIN_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+
+//
+//	判断是否64位系统
+//
+bool	Win_Is64bitsSystem();
+
+//
+//	根据类名获取子窗口，同 FindWindowEx
+//
+HWND	Win_FindChildByClass(HWND hWnd, const char* sClass);
+
+#endif
