4579d263caf10fd8666988bd6c74eb47750be09a
ximenpo <ximenpo@jiandan.ren>
2016-06-24 18:32:21 +0800

    add IEHostWindow.cpp/h

---
+++ inc/simple-win32/atl/IEHostWindow.cpp
@@ -0,0 +1,64 @@
+#include	<atlbase.h>
+#include	<atlhost.h>
+
+#include	"IEHostWindow.h"
+
+#include	"simple-win32/patcher.h"
+
+//////////////////////////////////////////////////////////
+static	Patcher*	gs_pacher_CreateInstance	= NULL;
+//////////////////////////////////////////////////////////
+
+IEHostWindow::IEHostWindow(void) {
+}
+
+
+IEHostWindow::~IEHostWindow(void) {
+}
+
+void IEHostWindow::patch_atl_creator_CAxHostWindow(FUNC_CreateInstance new_func, FUNC_CreateInstance* old_func) {
+    unpatch_atl_creator_CAxHostWindow();
+
+    FUNC_CreateInstance	xold;
+    gs_pacher_CreateInstance	= new Patcher(
+        &CAxHostWindow::_CreatorClass::CreateInstance,
+        (NULL != new_func ? new_func : &IEHostWindow::_CreatorClass::CreateInstance),
+        &xold,
+        true,
+        false
+    );
+    if(NULL != old_func) {
+        *old_func	= xold;
+    }
+}
+
+void IEHostWindow::unpatch_atl_creator_CAxHostWindow() {
+    if(NULL != gs_pacher_CreateInstance) {
+        if(gs_pacher_CreateInstance->patched()) {
+            gs_pacher_CreateInstance->remove_patch();
+        }
+        delete	gs_pacher_CreateInstance;
+        gs_pacher_CreateInstance	= NULL;
+    }
+}
+
+HRESULT STDMETHODCALLTYPE	IEHostWindow::ShowMessage(
+    /* [in] */ HWND hwnd,
+    /* [in] */ LPOLESTR lpstrText,
+    /* [in] */ LPOLESTR lpstrCaption,
+    /* [in] */ DWORD dwType,
+    /* [in] */ LPOLESTR lpstrHelpFile,
+    /* [in] */ DWORD dwHelpContext,
+    /* [out] */ LRESULT *plResult) {
+    return	S_FALSE;
+}
+
+HRESULT STDMETHODCALLTYPE	IEHostWindow::ShowHelp(
+    /* [in] */ HWND hwnd,
+    /* [in] */ LPOLESTR pszHelpFile,
+    /* [in] */ UINT uCommand,
+    /* [in] */ DWORD dwData,
+    /* [in] */ POINT ptMouse,
+    /* [out] */ IDispatch *pDispatchObjectHit) {
+    return	S_FALSE;
+}
---
+++ inc/simple-win32/atl/IEHostWindow.h
@@ -0,0 +1,63 @@
+#pragma once
+
+class IEHostWindow
+    :	public	CAxHostWindow
+    ,	public	IDocHostShowUI {
+public:
+    IEHostWindow(void);
+    ~IEHostWindow(void);
+
+public:
+    typedef	HRESULT (WINAPI *FUNC_CreateInstance)(
+        _In_opt_ void* pv,
+        _In_ REFIID riid,
+        _Deref_out_ LPVOID* ppv);
+
+    static	void	patch_atl_creator_CAxHostWindow(FUNC_CreateInstance new_func = NULL, FUNC_CreateInstance* old_func = NULL);
+    static	void	unpatch_atl_creator_CAxHostWindow();
+
+public:
+    DECLARE_POLY_AGGREGATABLE(IEHostWindow);
+
+    BEGIN_COM_MAP(IEHostWindow)
+    COM_INTERFACE_ENTRY2(IDispatch, IAxWinAmbientDispatchEx)
+    COM_INTERFACE_ENTRY(IAxWinHostWindow)
+    COM_INTERFACE_ENTRY(IAxWinHostWindowLic)
+    COM_INTERFACE_ENTRY(IOleClientSite)
+    COM_INTERFACE_ENTRY(IOleInPlaceSiteWindowless)
+    COM_INTERFACE_ENTRY(IOleInPlaceSiteEx)
+    COM_INTERFACE_ENTRY(IOleInPlaceSite)
+    COM_INTERFACE_ENTRY(IOleWindow)
+    COM_INTERFACE_ENTRY(IOleControlSite)
+    COM_INTERFACE_ENTRY(IOleContainer)
+    COM_INTERFACE_ENTRY(IObjectWithSite)
+    COM_INTERFACE_ENTRY(IServiceProvider)
+    COM_INTERFACE_ENTRY(IAxWinAmbientDispatchEx)
+    COM_INTERFACE_ENTRY(IAxWinAmbientDispatch)
+#ifndef _ATL_NO_DOCHOSTUIHANDLER
+    COM_INTERFACE_ENTRY(IDocHostUIHandler)
+#endif
+    COM_INTERFACE_ENTRY(IAdviseSink)
+    COM_INTERFACE_ENTRY(IDocHostShowUI)
+    END_COM_MAP()
+
+protected:
+    // IDocHostShowUI Impls.
+    virtual HRESULT STDMETHODCALLTYPE ShowMessage(
+        /* [in] */ HWND hwnd,
+        /* [in] */ LPOLESTR lpstrText,
+        /* [in] */ LPOLESTR lpstrCaption,
+        /* [in] */ DWORD dwType,
+        /* [in] */ LPOLESTR lpstrHelpFile,
+        /* [in] */ DWORD dwHelpContext,
+        /* [out] */ LRESULT *plResult);
+
+    virtual HRESULT STDMETHODCALLTYPE ShowHelp(
+        /* [in] */ HWND hwnd,
+        /* [in] */ LPOLESTR pszHelpFile,
+        /* [in] */ UINT uCommand,
+        /* [in] */ DWORD dwData,
+        /* [in] */ POINT ptMouse,
+        /* [out] */ IDispatch *pDispatchObjectHit);
+};
+
