05f695e3bb584afc72733e24fc6f1074d1316b3e
ximenpo <ximenpo@jiandan.ren>
2016-06-27 14:32:25 +0800

    add MAC address support.

---
+++ inc/simple-win32/mac.cpp
@@ -0,0 +1,105 @@
+﻿#include	<memory>
+
+#include	<windows.h>
+
+#include	"mac.h"
+
+#include	"simple/string.h"
+
+bool	Mac_EnumAdapterInfo(MAC_ADAPTER_ENUM_FUNC enum_func, void* user_data) {
+    IP_ADAPTER_INFO	dummy_mac_info	= {0};
+
+    DWORD	dwLen	= sizeof(IP_ADAPTER_INFO);
+    PIP_ADAPTER_INFO	pAdapterInfo	= &dummy_mac_info;
+    if(ERROR_BUFFER_OVERFLOW == GetAdaptersInfo(pAdapterInfo, &dwLen)) {
+        pAdapterInfo	= (PIP_ADAPTER_INFO)new char[dwLen];
+        if(ERROR_SUCCESS != GetAdaptersInfo(pAdapterInfo, &dwLen)) {
+            delete	(char*)pAdapterInfo;
+            return	false;
+        }
+    }
+
+    bool	result	= false;
+    while(NULL != pAdapterInfo) {
+        if(enum_func(pAdapterInfo, user_data)) {
+            result	= true;
+            break;
+        }
+        pAdapterInfo	= pAdapterInfo->Next;
+    }
+
+    if(&dummy_mac_info != pAdapterInfo) {
+        delete	(char*)pAdapterInfo;
+    }
+    return	result;
+}
+
+bool	Mac_IsPhysicalAdapter(PIP_ADAPTER_INFO adapter_info) {
+    if(NULL == adapter_info) {
+        return	false;
+    }
+
+    HKEY	hRoot;
+    if(ERROR_SUCCESS != RegOpenKeyExA(
+                HKEY_LOCAL_MACHINE,
+                "SYSTEM\\CurrentControlSet\\Control\\Class\\{4D36E972-E325-11CE-BFC1-08002bE10318}",
+                0,
+                KEY_READ,
+                &hRoot)) {
+        return	false;
+    }
+
+    bool	result_is_physical_adapter	= false;
+
+    char	subKey[MAX_PATH + 1];
+    DWORD	keyLen	= sizeof(subKey);
+    DWORD	dwIndex	= 0;
+    while(RegEnumKeyExA(hRoot, dwIndex, subKey, &keyLen, 0, 0, 0, 0) != ERROR_NO_MORE_ITEMS) {
+        keyLen	= sizeof(subKey);
+        dwIndex++;
+
+        HKEY	hSubKey;
+        if(ERROR_SUCCESS == RegOpenKeyExA(hRoot, subKey, 0, KEY_READ, &hSubKey)) {
+            char	value[MAX_PATH + 1]	= {0};
+            DWORD	value_len	= sizeof(value);
+            if(	ERROR_SUCCESS == RegQueryValueExA(hSubKey, "NetCfgInstanceId", NULL, NULL, (LPBYTE)value, &value_len)
+                    &&	0 == _strcmpi(value, adapter_info->AdapterName)
+              ) {
+                memset(value, 0, sizeof(value));
+                value_len	= sizeof(value);
+                if(	ERROR_SUCCESS == RegQueryValueExA(hSubKey, "Characteristics", NULL, NULL, (LPBYTE)&value, &value_len)
+                        &&	4 == ((*(DWORD*)value) & 4)// 4 -> NCF_PHYSICAL
+                  ) {
+                    result_is_physical_adapter	= true;
+                }
+            }
+            RegCloseKey(hSubKey);
+        }
+
+        if(result_is_physical_adapter) {
+            break;
+        }
+    }
+    RegCloseKey(hRoot);
+
+    return	result_is_physical_adapter;
+}
+
+static	bool	Mac_ENUM_IsPhysicalAdapterMac(PIP_ADAPTER_INFO info, void* user_data) {
+    if(!Mac_IsPhysicalAdapter(info)) {
+        return	false;
+    }
+
+    std::string*	mac	= (std::string*)user_data;
+    for(UINT i = 0; i < info->AddressLength; ++i) {
+        mac->append(string_hex(info->Address[i], false));
+    }
+
+    return	true;
+}
+
+std::string		Mac_GetPhysicalAdapterMac() {
+    std::string	mac;
+    Mac_EnumAdapterInfo(Mac_ENUM_IsPhysicalAdapterMac, &mac);
+    return	mac;
+}
---
+++ inc/simple-win32/mac.h
@@ -0,0 +1,18 @@
+﻿#ifndef MAC_H_WIN_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+#define MAC_H_WIN_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+
+#include	<string>
+
+#include	<Iphlpapi.h>
+
+typedef	bool	(*MAC_ADAPTER_ENUM_FUNC)(PIP_ADAPTER_INFO info, void* user_data);
+
+//	enum the net-cards' info.
+bool			Mac_EnumAdapterInfo(MAC_ADAPTER_ENUM_FUNC enum_func, void* user_data);
+
+//	is Physical net card or not.
+bool			Mac_IsPhysicalAdapter(PIP_ADAPTER_INFO adapter_info);
+
+std::string		Mac_GetPhysicalAdapterMac();
+
+#endif
